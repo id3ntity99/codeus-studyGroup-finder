47cb0154f542a165ffdba1cee7e5dd635e095ae1
id3ntity99 <jas06113@gmail.com>
2022-03-14 16:35:19 +0900

    chores: 서버 실행 shell script 수정

--- runDjangoServer.sh
+++ runDjangoServer.sh
@@ -1,1 +1,7 @@
-python3 $(pwd)/manage.py runserver
+if [[ "$VIRTUAL_ENV" != "" ]]; then
+	echo "Virtual env is activated"
+	python3 $(pwd)/manage.py runserver
+else
+	printf "Virtual env is NOT activated\nUse 'source venv/bin/activate' command to activate venv.\n"
+fi
+
